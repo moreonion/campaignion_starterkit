eb1643301577b7745c4c1f53de3edc140d0b0f92
Matthias Weiss <matthias@more-onion.com>
2012-07-20 15:56:27 +0200

    renamed rubik theme to ae_admin theme.

---
+++ js/ae_admin.js
@@ -0,0 +1,43 @@
+/**
+ * Implementation of Drupal behavior.
+ */
+(function($) {
+Drupal.behaviors.ae_admin = {};
+Drupal.behaviors.ae_admin.attach = function(context) {
+  // If there are both main column and side column buttons, only show the main
+  // column buttons if the user scrolls past the ones to the side.
+  $('div.form:has(div.column-main div.form-actions):not(.ae_admin-processed)').each(function() {
+    var form = $(this);
+    var offset = $('div.column-side div.form-actions', form).height() + $('div.column-side div.form-actions', form).offset().top;
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > offset) {
+        $('div.column-main div.form-actions', form).show();
+      }
+      else {
+        $('div.column-main div.form-actions', form).hide();
+      }
+    });
+    form.addClass('ae_admin-processed');
+  });
+
+  $('a.toggler:not(.ae_admin-processed)', context).each(function() {
+    var id = $(this).attr('href').split('#')[1];
+    // Target exists, add click handler.
+    if ($('#' + id).size() > 0) {
+      $(this).click(function() {
+        toggleable = $('#' + id);
+        toggleable.toggle();
+        $(this).toggleClass('toggler-active');
+        return false;
+      });
+    }
+    // Target does not exist, remove click handler.
+    else {
+      $(this).addClass('toggler-disabled');
+      $(this).click(function() { return false; });
+    }
+    // Mark as processed.
+    $(this).addClass('ae_admin-processed');
+  });
+};
+})(jQuery);
