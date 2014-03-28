ab266c5bff726f119ff6293d87c436e55bb0ffeb
Roman Zimmermann <roman@more-onion.com>
2014-03-28 18:44:33 +0100

    Completely switch to new-style templates.

--- modules/campaignion_donation/lib/ActionType.php
+++ modules/campaignion_donation/lib/ActionType.php
@@ -6,15 +6,8 @@ use \Drupal\campaignion\Wizard\DonationWizard;
 
 class ActionType extends \Drupal\campaignion\Action\TypeBase {
   public function defaultTemplateNid() {
-    $query = new \EntityFieldQuery();
-
-    $result = $query->entityCondition('entity_type', 'node')
-    ->entityCondition('bundle', 'webform_template_type')
-    ->propertyCondition('title', 'Global 2000 Donation Webform Template')
-    ->execute();
-
-    $nids = isset($result['node']) ? array_keys($result['node']) : array(NULL);
-    return $nids[0];
+    $ids = entity_get_id_by_uuid('node', array('donation-template-default-en'));
+    return array_shift($ids);
   }
 
   public function wizard($node = NULL) {
--- modules/campaignion_flexible_form/lib/ActionType.php
+++ modules/campaignion_flexible_form/lib/ActionType.php
@@ -5,6 +5,11 @@ namespace Drupal\campaignion_flexible_form;
 use \Drupal\campaignion\Wizard\WebformWizard;
 
 class ActionType extends \Drupal\campaignion\Action\TypeBase {
+  public function defaultTemplateNid() {
+    $ids = entity_get_id_by_uuid('node', array('webform-template-minimal-en'));
+    return array_shift($ids);
+  }
+
   public function wizard($node = NULL) {
     return new WebformWizard($this->parameters, $node, $this->type);
   }
