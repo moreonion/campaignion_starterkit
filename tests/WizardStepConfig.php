f2d7eefc8ce6781e300546b659017233eb575767
phaer <phaer@nino.dev>
2014-09-19 14:50:10 +0200

    adapt test namespaces.

--- tests/WizardStepConfig.php
+++ tests/WizardStepConfig.php
@@ -7,7 +7,7 @@
 class WizardStepConfig {
   protected $testCase = NULL;
 
-  public function __construct(DrupalSeleniumTestCase $test_case) {
+  public function __construct(\Drupal\Tests\DrupalSeleniumTestCase $test_case) {
     $this->testCase = $test_case;
   }
 
