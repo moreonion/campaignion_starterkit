50a74053b9740d58a3b9c43b5422d1f487af639c
Roman Zimmermann <roman@more-onion.com>
2016-02-18 15:54:55 +0100

    Adjust template node for email_to_target actions.

--- modules/campaignion_email_to_target/src/ActionType.php
+++ modules/campaignion_email_to_target/src/ActionType.php
@@ -4,7 +4,7 @@ namespace Drupal\campaignion_email_to_target;
 
 class ActionType extends \Drupal\campaignion_action\TypeBase {
   public function defaultTemplateNid() {
-    $ids = \entity_get_id_by_uuid('node', array('2825470e-e582-414d-b01c-e6e71a028075'));
+    $ids = \entity_get_id_by_uuid('node', array('f5645542-33eb-445e-8e6b-8300cf385069'));
     return array_shift($ids);
   }
 
