f2b8c5d1776f8262f3aae082ff0fd18e8687f072
Roman Zimmermann <roman@more-onion.com>
2016-03-14 15:03:36 +0100

    Better error messages.

--- modules/campaignion_email_to_target/src/Wizard/FormStep.php
+++ modules/campaignion_email_to_target/src/Wizard/FormStep.php
@@ -63,21 +63,24 @@ class FormStep extends WebformStep {
       }
     }
     if ($postcode && $postcode['form_key'] != 'postcode') {
-      form_error($form, t("The postcode field must have the form_key 'postcode'"));
+      form_error($form, t('The postcode field has the wrong form key - it needs to be "postcode". Change it by clicking on the field, then click "edit" underneath Title field.'));
     }
     if ($postcode && $target_selector) {
-      if ($postcode['page_num'] >= $target_selector['page_num']) {
-        form_error($form, t("The 'target selector' must be at least one page after the 'postcode' field."));
+      if ($postcode['page_num'] == $target_selector['page_num']) {
+        form_error($form, t("The postcode and the target selector can't be placed on the same page. Add a 'page break' between the postcode field and the target selector field."));
+      }
+      elseif ($postcode['page_num'] > $target_selector['page_num']) {
+        form_error($form, t("The postcode field needs to be placed before the target selector field, with a page break in between."));
       }
     }
     elseif (!$postcode && !$target_selector) {
-      form_error($form, t("The form must have a 'UK Postcode' element and as well as a 'target selector'."));
+      form_error($form, t("The postcode field and the target selector field have to be available. Please add them to the form."));
     }
     elseif (!$postcode) {
-      form_error($form, t("There must be a 'UK Postcode' element on the form and it needs to be on a page before the 'target selector'."));
+      form_error($form, t("The postcode field needs to be in the form. Please drag it in."));
     }
     elseif (!$target_selector) {
-      form_error($form, t("There must be a 'Target selector' element on the form and it needs to be on a page after the 'target selector'."));
+      form_error($form, t("The target selector field needs to be in the form. Please drag it in."));
     }
   }
 
