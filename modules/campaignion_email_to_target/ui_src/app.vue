4cd7669da0949c72f3e1a615c7c74937acaafc55
Maya Kokits <hello@mayakokits.com>
2016-04-12 13:12:03 +0200

    [e2t-ui] set up vue + karma

---
+++ modules/campaignion_email_to_target/ui_src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+module.exports = {
+  replace: false,
+  data: function() {
+    return {
+      msg: 'hello '
+    };
+  },
+  ready: function() {
+    console.log('ready');
+    this.msg += 'world!';
+  }
+}
+</script>
+
+<style>
+
+</style>
