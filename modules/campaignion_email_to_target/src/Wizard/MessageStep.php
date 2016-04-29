1d87f5ef851c1b797c62a6431af98e7b912d7f18
Roman Zimmermann <roman@more-onion.com>
2016-04-29 19:18:59 +0200

    Migrate from field to custom tables.

--- modules/campaignion_email_to_target/src/Wizard/MessageStep.php
+++ modules/campaignion_email_to_target/src/Wizard/MessageStep.php
@@ -3,6 +3,7 @@
 namespace Drupal\campaignion_email_to_target\Wizard;
 
 use \Drupal\campaignion\Forms\EntityFieldForm;
+use \Drupal\campaignion_email_to_target\MessageTemplate;
 
 class MessageStep extends \Drupal\campaignion_wizard\WizardStep {
   protected $step  = 'message';
@@ -10,9 +11,46 @@ class MessageStep extends \Drupal\campaignion_wizard\WizardStep {
 
   public function stepForm($form, &$form_state) {
     $form = parent::stepForm($form, $form_state);
-    $field = $this->wizard->parameters['email_to_target']['message_field'];
-    $this->fieldForm = new EntityFieldForm('node', $this->wizard->node, [$field]);
-    $form += $this->fieldForm->formArray($form_state);
+    $form['messages'] = [
+      '#type' => 'container',
+      '#title' => t('Message that will be sent to target(s)'),
+      '#id' => drupal_html_id('email-to-target-messages-widget'),
+      '#attributes' => ['class' => ['email-to-target-messages-widget']],
+      'app-tag' => [
+        '#markup' => '<app></app>'
+      ],
+    ];
+
+    $info = token_get_info();
+    $tokens = [];
+    foreach(['email-to-target', 'webform-tokens'] as $type) {
+      if (!isset($info['types'][$type])) {
+        continue;
+      }
+      $type_info = $info['types'][$type];
+      $tokens[$type] = [
+        'name' => $type_info['name'],
+        'description' => $type_info['description'],
+        'tokens' => [],
+      ];
+      foreach ($info['tokens'][$type] as $key => $token) {
+        $tokens[$type]['tokens'][] = [
+          'token' => $key,
+          'name' => $token['name'],
+          'description' => $token['description'],
+        ];
+      }
+    }
+    $settings['tokens'] = $tokens;
+
+    $settings['messages'] = [];
+    foreach (MessageTemplate::byNid($this->wizard->node->nid) as $m) {
+      $settings['messages'][] = $m->toArray();
+    }
+
+    $settings = ['emailToTarget' => $settings];
+    $form['messages']['#attached']['js'][] = ['data' => $settings, 'type' => 'setting'];
+    $form['messages']['#attached']['js'][] = ['data' => drupal_get_path('module', 'campaignion_email_to_target') . '/js/messages_widget.js', 'scope' => 'footer'];
     return $form;
   }
 
