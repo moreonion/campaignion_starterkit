72f27768aa75c34ffc1e060742305393c07ef8e3
Maya Kokits <hello@mayakokits.com>
2016-05-17 10:19:52 +0200

    [messages widget] add helptexts

--- modules/campaignion_email_to_target/ui_src/components/message-editor.vue
+++ modules/campaignion_email_to_target/ui_src/components/message-editor.vue
@@ -4,22 +4,22 @@
     <div class="form-group">
       <label for="message-subject-{{ _uid }}">Subject <a href="#" @click.prevent="toggleHelpText('subject')" class="show-help-text"><span>?</span></a></label>
       <input type="text" v-model="message.subject" data-token-insertable class="form-control" id="message-subject-{{ _uid }}">
-      <small v-if="helpText['subject']" class="text-muted">Helptext goes here.</small>
+      <small v-if="helpText['subject']" class="text-muted">This is the subject line of the message that will be sent to the target.</small>
     </div>
     <div class="form-group">
       <label for="message-header-{{ _uid }}">Header <a href="#" @click.prevent="toggleHelpText('header')" class="show-help-text"><span>?</span></a></label>
       <textarea rows="3" v-model="message.header" data-token-insertable class="form-control" id="message-header-{{ _uid }}"></textarea>
-      <small v-if="helpText['header']" class="text-muted">Helptext goes here.</small>
+      <small v-if="helpText['header']" class="text-muted">This part of the message will not be editable by your supporters.</small>
     </div>
     <div class="form-group">
       <label for="message-body-{{ _uid }}">Body <a href="#" @click.prevent="toggleHelpText('body')" class="show-help-text"><span>?</span></a></label>
       <textarea rows="6" v-model="message.body" data-token-insertable class="form-control" id="message-body-{{ _uid }}"></textarea>
-      <small v-if="helpText['body']" class="text-muted">Helptext goes here.</small>
+      <small v-if="helpText['body']" class="text-muted">This is the main part of the message that will be sent to the target. If you have chosen to make the message editable by your supporters, they will be able to edit this part of the message.</small>
     </div>
     <div class="form-group">
       <label for="message-footer-{{ _uid }}">Footer <a href="#" @click.prevent="toggleHelpText('footer')" class="show-help-text"><span>?</span></a></label>
       <textarea rows="3" v-model="message.footer" data-token-insertable class="form-control" id="message-footer-{{ _uid }}"></textarea>
-      <small v-if="helpText['footer']" class="text-muted">Helptext goes here.</small>
+      <small v-if="helpText['footer']" class="text-muted">This part of the message will not be editable by your supporters.</small>
     </div>
   </fieldset>
 </template>
