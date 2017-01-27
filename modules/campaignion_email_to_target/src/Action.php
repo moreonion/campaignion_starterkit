cedbffc63d57f7408d79259d4eb55a3d7ba0a1ff
Roman Zimmermann <roman@more-onion.com>
2017-01-27 16:30:39 +0100

    phpcs fixes.

--- modules/campaignion_email_to_target/src/Action.php
+++ modules/campaignion_email_to_target/src/Action.php
@@ -2,9 +2,19 @@
 
 namespace Drupal\campaignion_email_to_target;
 
-use \Drupal\little_helpers\Webform\Submission;
+use \Drupal\campaignion_action\ActionBase;
+use \Drupal\campaignion_email_to_target\Api\Client;
 
-class Action extends \Drupal\campaignion_action\ActionBase {
+/**
+ * Defines special behavior for email to target actions.
+ *
+ * Mainly deals with the configuration and with selecting messages / exclusion.
+ */
+class Action extends ActionBase {
+
+  /**
+   * Choose an appropritae exclusion for a given target.
+   */
   public function getExclusion($constituency) {
     foreach (MessageTemplate::byNid($this->node->nid) as $t) {
       if ($t->type == 'exclusion' && $t->checkFilters([], $constituency)) {
@@ -13,6 +23,9 @@ class Action extends \Drupal\campaignion_action\ActionBase {
     }
   }
 
+  /**
+   * Choose an appropriate message for a given target.
+   */
   public function getMessage($target, $constituency) {
     $templates = MessageTemplate::byNid($this->node->nid);
     foreach ($templates as $t) {
@@ -24,17 +37,26 @@ class Action extends \Drupal\campaignion_action\ActionBase {
     return NULL;
   }
 
+  /**
+   * Get options for this action.
+   */
   public function getOptions() {
     $field = $this->type->parameters['email_to_target']['options_field'];
     $items = field_get_items('node', $this->node, $field);
     return $items ? $items[0] : ['dataset_name' => 'mp'];
   }
 
+  /**
+   * Get the selected dataset for this action.
+   */
   public function dataset() {
-    $api = Api\Client::fromConfig();
+    $api = Client::fromConfig();
     return $api->getDataset($this->getOptions()['dataset_name']);
   }
 
+  /**
+   * Create a link to view the action in test-mode.
+   */
   public function testLink($title, $query = [], $options = []) {
     return $this->_testLink($title, $query, $options);
   }
