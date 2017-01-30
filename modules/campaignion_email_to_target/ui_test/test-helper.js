c2e4c0464354db628b76cdc0807243e598a9baaf
Maya Kokits <maya@more-onion.com>
2017-01-30 16:37:12 +0100

    [messages widget] FIX: remove closing bracket and fix indentation

--- modules/campaignion_email_to_target/ui_test/test-helper.js
+++ modules/campaignion_email_to_target/ui_test/test-helper.js
@@ -1,86 +1,84 @@
 import Vue from 'vue'
 import dragula from '../ui_src/plugins/vue-dragula.js'
 
-  export function setup(app, data, options) {
-    global.Drupal = {
-      settings: {
-        campaignion_email_to_target: data
-      }
+export function setup(app, data, options) {
+  global.Drupal = {
+    settings: {
+      campaignion_email_to_target: data
     }
-
-    $('body').append($('<div id="app"></div>'))
-
-    if (typeof options === 'undefined') options = {}
-
-    $.extend(options, {
-      el: '#app',
-      template: '<div class="email-to-target-messages-widget e2tmw"><test></test></div>',
-      components: {
-        'test': app
-      },
-      ready: function() {
-        $(window).off('beforeunload') // karma does't like this event handler...
-      }
-    })
-
-    Vue.use(dragula);
-
-    var rootInstance = new Vue(options)
-
-    return rootInstance.$children[0]
   }
 
-  export function teardown(vm) {
-    vm.$root.$destroy(true);
-    $('body').empty()
-    global.Drupal = null
-  }
+  $('body').append($('<div id="app"></div>'))
 
-  export function triggerDragAndDrop(elemDrag, elemDrop) {
-    if (!elemDrag || !elemDrop) return false;
+  if (typeof options === 'undefined') options = {}
 
-    // function for triggering mouse events
-    var fireMouseEvent = function (type, elem, centerX, centerY) {
-      var evt = document.createEvent('MouseEvents');
-      evt.initMouseEvent(type, true, true, window, 1, 1, 1, centerX, centerY, false, false, false, false, 0, elem);
-      elem.dispatchEvent(evt);
-    };
+  $.extend(options, {
+    el: '#app',
+    template: '<div class="email-to-target-messages-widget e2tmw"><test></test></div>',
+    components: {
+      'test': app
+    },
+    ready: function() {
+      $(window).off('beforeunload') // karma does't like this event handler...
+    }
+  })
 
-    // calculate positions
-    var pos = elemDrag.getBoundingClientRect();
-    var center1X = Math.floor((pos.left + pos.right) / 2);
-    var center1Y = Math.floor((pos.top + pos.bottom) / 2);
-    pos = elemDrop.getBoundingClientRect();
-    var center2X = Math.floor((pos.left + pos.right) / 2);
-    var center2Y = Math.floor((pos.top + pos.bottom) / 2);
+  Vue.use(dragula);
 
-    // mouse over dragged element and mousedown
-    fireMouseEvent('mousemove', elemDrag, center1X, center1Y);
-    fireMouseEvent('mouseenter', elemDrag, center1X, center1Y);
-    fireMouseEvent('mouseover', elemDrag, center1X, center1Y);
-    fireMouseEvent('mousedown', elemDrag, center1X, center1Y);
+  var rootInstance = new Vue(options)
 
-    // start dragging process over to drop target
-    fireMouseEvent('dragstart', elemDrag, center1X, center1Y);
-    fireMouseEvent('drag', elemDrag, center1X, center1Y);
-    fireMouseEvent('mousemove', elemDrag, center1X, center1Y);
-    fireMouseEvent('drag', elemDrag, center2X, center2Y);
-    fireMouseEvent('mousemove', elemDrop, center2X, center2Y);
+  return rootInstance.$children[0]
+}
 
-    // trigger dragging process on top of drop target
-    fireMouseEvent('mouseenter', elemDrop, center2X, center2Y);
-    fireMouseEvent('dragenter', elemDrop, center2X, center2Y);
-    fireMouseEvent('mouseover', elemDrop, center2X, center2Y);
-    fireMouseEvent('dragover', elemDrop, center2X, center2Y);
+export function teardown(vm) {
+  vm.$root.$destroy(true);
+  $('body').empty()
+  global.Drupal = null
+}
 
-    // release dragged element on top of drop target
-    fireMouseEvent('drop', elemDrop, center2X, center2Y);
-    fireMouseEvent('dragend', elemDrag, center2X, center2Y);
-    fireMouseEvent('mouseup', elemDrag, center2X, center2Y);
+export function triggerDragAndDrop(elemDrag, elemDrop) {
+  if (!elemDrag || !elemDrop) return false;
 
-    return true;
-  }
+  // function for triggering mouse events
+  var fireMouseEvent = function (type, elem, centerX, centerY) {
+    var evt = document.createEvent('MouseEvents');
+    evt.initMouseEvent(type, true, true, window, 1, 1, 1, centerX, centerY, false, false, false, false, 0, elem);
+    elem.dispatchEvent(evt);
+  };
 
+  // calculate positions
+  var pos = elemDrag.getBoundingClientRect();
+  var center1X = Math.floor((pos.left + pos.right) / 2);
+  var center1Y = Math.floor((pos.top + pos.bottom) / 2);
+  pos = elemDrop.getBoundingClientRect();
+  var center2X = Math.floor((pos.left + pos.right) / 2);
+  var center2Y = Math.floor((pos.top + pos.bottom) / 2);
+
+  // mouse over dragged element and mousedown
+  fireMouseEvent('mousemove', elemDrag, center1X, center1Y);
+  fireMouseEvent('mouseenter', elemDrag, center1X, center1Y);
+  fireMouseEvent('mouseover', elemDrag, center1X, center1Y);
+  fireMouseEvent('mousedown', elemDrag, center1X, center1Y);
+
+  // start dragging process over to drop target
+  fireMouseEvent('dragstart', elemDrag, center1X, center1Y);
+  fireMouseEvent('drag', elemDrag, center1X, center1Y);
+  fireMouseEvent('mousemove', elemDrag, center1X, center1Y);
+  fireMouseEvent('drag', elemDrag, center2X, center2Y);
+  fireMouseEvent('mousemove', elemDrop, center2X, center2Y);
+
+  // trigger dragging process on top of drop target
+  fireMouseEvent('mouseenter', elemDrop, center2X, center2Y);
+  fireMouseEvent('dragenter', elemDrop, center2X, center2Y);
+  fireMouseEvent('mouseover', elemDrop, center2X, center2Y);
+  fireMouseEvent('dragover', elemDrop, center2X, center2Y);
+
+  // release dragged element on top of drop target
+  fireMouseEvent('drop', elemDrop, center2X, center2Y);
+  fireMouseEvent('dragend', elemDrag, center2X, center2Y);
+  fireMouseEvent('mouseup', elemDrag, center2X, center2Y);
+
+  return true;
 }
 
 $.fn.selectRange = function(start, end) {
