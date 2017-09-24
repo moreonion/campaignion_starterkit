03c1c72f6967484e00c6ae2b232b8197136be792
Roman Zimmermann <roman@more-onion.com>
2017-09-24 09:23:35 +0200

    Simplify protest target importer.

--- modules/campaignion_protest_target_contact_type/src/ProtestTarget.php
+++ modules/campaignion_protest_target_contact_type/src/ProtestTarget.php
@@ -10,23 +10,8 @@ class ProtestTarget implements ContactTypeInterface {
   public function __construct() {}
   public function importer($source) {
     $mappings = array(
-      new Field\Field('field_gender',     'gender'),
-      new Field\Field('field_salutation', 'salutation'),
-      new Field\Field('field_title',      'title'),
       new Field\Field('first_name'),
       new Field\Field('last_name'),
-      new Field\Date('field_date_of_birth',    'date_of_birth'),
-      new Field\Address('field_address', array(
-        'thoroughfare'        => 'street_address',
-        'postal_code'         => 'zip_code',
-        'locality'            => 'city',
-        'administrative_area' => 'state',
-        'country'             => 'country',
-      )),
-      new Field\Phone('field_phone_number', 'phone_number'),
-      new Field\Phone('field_phone_number', 'mobile_number'),
-      new Field\EmailBulk('redhen_contact_email', 'email', 'email_newsletter'),
-      new Field\Field('field_direct_mail_newsletter', 'direct_mail_newsletter'),
     );
     return new ImporterBase($mappings, 'email_protest_target');
   }
