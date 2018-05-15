c6b4464170f7098e7ee7ae21dc4b0347151da530
Michael Strasser <mst1409@gmx.at>
2018-05-15 22:45:15 +0200

    Implemented preprocessing functions and theme templates

---
+++ modules/campaignion_overlay/campaignion-overlay-content.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Provides a template for the `campaignion_overlay_content` field.
+ */
+?>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print render($campaignion_overlay_form); ?>
+</div>
---
+++ modules/campaignion_overlay/campaignion-overlay.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Displays a webform in an overlay.
+ */
+?>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print render($items); ?>
+</div>
